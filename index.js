06e507faa0c5e9c8247eb3f918ed6a6fbf9cbd49
michael2023-code <michael.mutunga@student.moringaschool.com>
2023-07-14 09:56:38 +0300

    functions lab continuation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(doEvent ="roller-skate"){
+return `This Saturday, I want to ${doEvent}!`
+}
+
+saturdayFun();
+
+function mondayWork(event= 'go to the office'){
+    return`This Monday, I will ${event}.`
+}
+
+mondayWork(); 
+
+function wrapAdjective(symbol ="#"){
+return function(adjective ="loving"){
+    return `You are ${symbol}${adjective}${symbol}!`
+
+}
+}
